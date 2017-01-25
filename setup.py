ab0b1201fbea0535b8539c30d73c01b35424648c
Simon Fraser <sfraser@mozilla.com>
2017-01-25 11:49:37 +0000

    counting the number of releases is tricky
    We only wanted to count when submitting, but it's chunked by then, so the counter increments too quickly. The `to_mar == from_mar` check should catch the condition we're concerned about

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="funsize",
-    version="0.69",
+    version="0.70",
     description="Funsize Scheduler",
     author="Mozilla Release Engineering",
     packages=["funsize"],
